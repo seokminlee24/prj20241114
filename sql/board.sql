04c68993e7cd529513607a9a9d8de564e59af8e0
seokminlee24 <lsm67286564@gmail.com>
2024-11-14 11:30:51 +0900

    게시물 테이블 board 생성

---
+++ sql/board.sql
@@ -0,0 +1,11 @@
+# 게시물 테이블 생성
+USE prj20241114;
+
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(300)  NOT NULL,
+    content  VARCHAR(5000) NOT NULL,
+    writer   varchar(100)  NOT NULL,
+    inserted DATETIME DEFAULT NOW()
+);
