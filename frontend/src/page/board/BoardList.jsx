9d1205525ae066d7e24cf309fb22c8d5990dd077
seokminlee24 <lsm67286564@gmail.com>
2024-11-14 12:44:23 +0900

    게시물 목록 보기를 테이블로

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Table, TableRoot } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -16,11 +16,31 @@ export function BoardList() {
     <Box>
       <h3>게시물 목록</h3>
       {/*    게시물들을 테이블로*/}
-      {boardList.map((board) => (
+      {/*{boardList.map((board) => (
         <li>
           {board.title},{board.writer}
         </li>
-      ))}
+      ))}*/}
+      <TableRoot interactive>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>번호</Table.ColumnHeader>
+            <Table.ColumnHeader>제목</Table.ColumnHeader>
+            <Table.ColumnHeader>작성자</Table.ColumnHeader>
+            <Table.ColumnHeader>작성일자</Table.ColumnHeader>
+          </Table.Row>
+          <Table.Body>
+            {boardList.map((board) => (
+              <Table.Row>
+                <Table.Cell>{board.id}</Table.Cell>
+                <Table.Cell>{board.title}</Table.Cell>
+                <Table.Cell>{board.writer}</Table.Cell>
+                <Table.Cell>{board.inserted}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Header>
+      </TableRoot>
     </Box>
   );
 }
