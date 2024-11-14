6f491249c40be8a624896a5366f73e268fa4b445
seokminlee24 <lsm67286564@gmail.com>
2024-11-14 10:15:30 +0900

    react 파일 정리
    필요없는 파일 삭제
    차크라 ui 설정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Toaster } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+      <Toaster />
+    </Provider>
   </StrictMode>,
-)
+);
