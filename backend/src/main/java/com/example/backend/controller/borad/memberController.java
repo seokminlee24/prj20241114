64c95dfc12da2bbefc708bc72ae5436c3ba75537
seokminlee24 <lsm67286564@gmail.com>
2024-11-15 15:42:00 +0900

    회원 가입 작업 중

---
+++ backend/src/main/java/com/example/backend/controller/borad/memberController.java
@@ -0,0 +1,21 @@
+package com.example.backend.controller.borad;
+
+import com.example.backend.dto.member.Member;
+import com.example.backend.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class memberController {
+    final MemberService service;
+
+    @PostMapping("signup")
+    public void signup(@RequestBody Member member) {
+        System.out.println(member);
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BoardList } from "./page/board/BoardList.jsx";
 import { RootLayout } from "./page/root/RootLayout.jsx";
 import { BoardView } from "./page/board/BoardView.jsx";
 import { BoardEdit } from "./page/board/BoardEdit.jsx";
+import { MemberSignup } from "./page/member/MemberSignup.jsx";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,26 @@ const router = createBrowserRouter([
         element: <BoardView />,
       },
       { path: "edit/:id", element: <BoardEdit /> },
+      {
+        path: "member/signup",
+        element: (
+          <div>
+            <MemberSignup />
+          </div>
+        ),
+      },
+      {
+        path: "member/list",
+        element: <div>회원목록</div>,
+      },
+      {
+        path: "member/:id",
+        element: <div>회원정보</div>,
+      },
+      {
+        path: "member/edit/:id",
+        element: <div>회원정보수정</div>,
+      },
     ],
   },
 ]);
