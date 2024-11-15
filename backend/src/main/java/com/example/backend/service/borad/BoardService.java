983b98e5045ee107b9f66dfa3ab5ad8b3fd65317
seokminlee24 <lsm67286564@gmail.com>
2024-11-15 10:53:49 +0900

    페이지 이동

--- backend/src/main/java/com/example/backend/service/borad/BoardService.java
+++ backend/src/main/java/com/example/backend/service/borad/BoardService.java
@@ -16,8 +16,8 @@ public class BoardService {
     final BoardMapper mapper;
 
 
-    public List<Board> list() {
-        return mapper.selectAll();
+    public List<Board> list(Integer page) {
+        return mapper.selectPage((page - 1) * 10);
     }
 
     public Board get(int id) {
--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,19 +1,34 @@
-import { Box, Table, TableRoot } from "@chakra-ui/react";
+import { Box, HStack, Table, TableRoot } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  PaginationItems,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+  PaginationRoot,
+} from "../../components/ui/pagination.jsx";
 
 export function BoardList() {
   //const [boardList, setBoardList] = useState("");
   const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     axios
-      .get("/api/board/list")
+      .get("/api/board/list", {
+        params: searchParams,
+      })
       .then((res) => res.data)
       .then((date) => setBoardList(date));
   }, []);
+
+  // searchParams
+  console.log(searchParams.toString());
+  // page 번호
+  const pageParam = searchParams.get("page") ? searchParams.get("page") : "1";
+  const page = Number(pageParam);
 
   function handleRowClick(id) {
     navigate(`/view/${id}`);
@@ -48,6 +63,15 @@ export function BoardList() {
           ))}
         </Table.Body>
       </TableRoot>
+      <Box>
+        <PaginationRoot count={1500} pageSize={10} page={page}>
+          <HStack>
+            <PaginationPrevTrigger />
+            <PaginationItems />
+            <PaginationNextTrigger />
+          </HStack>
+        </PaginationRoot>
+      </Box>
     </Box>
   );
 }
