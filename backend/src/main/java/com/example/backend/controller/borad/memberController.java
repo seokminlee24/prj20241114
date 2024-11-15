e1d77745cfc977928436ea5b21936545031f8554
seokminlee24 <lsm67286564@gmail.com>
2024-11-15 16:21:14 +0900

    회원 가입 성공/실패 토스트 출력

--- backend/src/main/java/com/example/backend/controller/borad/memberController.java
+++ backend/src/main/java/com/example/backend/controller/borad/memberController.java
@@ -3,10 +3,14 @@ package com.example.backend.controller.borad;
 import com.example.backend.dto.member.Member;
 import com.example.backend.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +19,18 @@ public class memberController {
     final MemberService service;
 
     @PostMapping("signup")
-    public void signup(@RequestBody Member member) {
-        service.add(member);
+    public ResponseEntity<Map<String, Object>> signup(@RequestBody Member member) {
+        try {
+            if (service.add(member)) {
+                return ResponseEntity.ok().body(Map.of("message", Map.of("type", "success",
+                        "text", "회원 가입되었습니다.")));
+            } else {
+                return ResponseEntity.ok().body(Map.of("message", Map.of("type", "error",
+                        "text", "회원 가입중 문제가 발생했습니다.")));
+            }
+        } catch (DuplicateKeyException e) {
+            return ResponseEntity.internalServerError().body(Map.of("message",
+                    Map.of("type", "error", "text", "이미 존재하는 아이디입니다.")));
+        }
     }
 }
