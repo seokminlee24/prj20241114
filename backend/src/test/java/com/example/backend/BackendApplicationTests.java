615c1b81a2776225eb3536d71e16089500849a6b
seokminlee24 <lsm67286564@gmail.com>
2024-11-19 11:28:34 +0900

    spring security 설정

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
