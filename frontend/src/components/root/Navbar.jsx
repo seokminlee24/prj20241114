7519a5e1f30def66eefd9796cff0daaa98517210
seokminlee24 <lsm67286564@gmail.com>
2024-11-19 16:36:53 +0900

    navbar 에서 분해할당으로 코드 정리

--- frontend/src/components/root/Navbar.jsx
+++ frontend/src/components/root/Navbar.jsx
@@ -4,40 +4,41 @@ import { useContext } from "react";
 import {AuthenticationContext} from "../content/AuthenticationProvider.jsx";
 
 
+
 export function Navbar() {
     const navigate = useNavigate();
 
     // step 2 : context 사용하기
-    const authentication = useContext(AuthenticationContext);
+    const {id, isAdmin, isAuthenticated, logout} = useContext(AuthenticationContext)
 
     return (
         <Flex gap={3}>
             <Box onClick={() => navigate("/")}>HOME</Box>
-            {authentication.isAuthenticated && (
+            {isAuthenticated && (
                 <Box onClick={() => navigate("/add")}>작성</Box>
             )}
-            {authentication.isAuthenticated || (
+            {isAuthenticated || (
                 <Box onClick={() => navigate("/member/signup")}>가입</Box>
             )}
 
-            {authentication.isAdmin && (
+            {isAdmin && (
                 <Box onClick={() => navigate("/member/list")}>회원목록</Box>
             )}
 
-            {authentication.isAuthenticated || (
+            {isAuthenticated || (
                 <Box onClick={() => navigate("/member/login")}>로그인</Box>
             )}
-            {authentication.isAuthenticated && (
+            {isAuthenticated && (
                 <Box
                     onClick={() => {
-                        authentication.logout();
+                        logout();
                         navigate("/member/login");
                     }}
                 >
                     로그아웃
                 </Box>
             )}
-            <Box>{authentication.id}</Box>
+            <Box>{id}</Box>
         </Flex>
     );
 }
