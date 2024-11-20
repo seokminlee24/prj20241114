87ac1655e0f6e58837b6bc0687a1ad6bd3f4a99a
seokminlee24 <lsm67286564@gmail.com>
2024-11-20 09:24:53 +0900

    자신의 글에만 수정 삭제 버튼 보이게

--- frontend/src/page/board/BoardEdit.jsx
+++ frontend/src/page/board/BoardEdit.jsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { Box, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Field } from "../../components/ui/field.jsx";
 import { Button } from "../../components/ui/button.jsx";
@@ -15,108 +15,113 @@ import {
   DialogTrigger,
 } from "../../components/ui/dialog.jsx";
 import { toaster } from "../../components/ui/toaster.jsx";
+import {AuthenticationContext} from "../../components/content/AuthenticationProvider.jsx";
 
 export function BoardEdit() {
   const [board, setBoard] = useState(null);
-  const { id } = useParams();
   const [progress, setProgress] = useState(false);
   const [dialogOpen, setDialogOpen] = useState(false);
 
+  const { hasAccess } = useContext(AuthenticationContext);
+
+  const { id } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios
-      .get(`/api/board/view/${id}`)
-      .then((res) => res.data)
-      .then((data) => setBoard(data));
+    axios.get(`/api/board/view/${id}`).then((res) => setBoard(res.data));
   }, []);
 
   const handleSaveClick = () => {
     setProgress(true);
+
     axios
-      .put("/api/board/update", board)
-      .then((res) => res.data)
-      .then((data) => {
-        toaster.create({
-          type: data.message.type,
-          description: data.message.text,
+        .put("/api/board/update", board)
+        .then((res) => res.data)
+        .then((data) => {
+          toaster.create({
+            type: data.message.type,
+            description: data.message.text,
+          });
+          navigate(`/view/${board.id}`);
+        })
+        .catch((e) => {
+          const message = e.response.data.message;
+          toaster.create({
+            type: message.type,
+            description: message.text,
+          });
+        })
+        .finally(() => {
+          setProgress(false);
+          setDialogOpen(false);
         });
-        navigate(`/view/${board.id}`);
-      })
-      .catch((e) => {
-        const message = e.response.message;
-        toaster.create({
-          type: data.message.type,
-          description: data.message.text,
-        });
-      })
-      .finally(() => {
-        setProgress(false);
-        setDialogOpen(false);
-      });
   };
-  // board가 null일 떄 (첫 렌더)
+
+  // board가 null일 때 (첫 렌더)
   if (board === null) {
     return <Spinner />;
   }
 
   // 제목이나 본문이 비어있는 지 확인
   const disabled = !(
-    board.title.trim().length > 0 && board.content.trim().length > 0
+      board.title.trim().length > 0 && board.content.trim().length > 0
   );
 
   return (
-    <Box>
-      <h3>{id}번 게시물 수정</h3>
-
-      <Stack gap={5}>
-        <Field label={"제목"}>
-          <Input
-            value={board.title}
-            onChange={(e) => setBoard({ ...board, title: e.target.value })}
-          />
-        </Field>
-        <Field label={"본문"}>
-          <Textarea
-            value={board.content}
-            onChange={(e) => setBoard({ ...board, content: e.target.value })}
-          />
-        </Field>
-        <DialogRoot
-          open={dialogOpen}
-          onOpenChange={(e) => setDialogOpen(e.open)}
-        >
-          <DialogTrigger asChild>
-            <Button
-              disabled={disabled}
-              colorPalette={"cyan"}
-              variant={"outline"}
-            >
-              저장
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>저장확인</DialogTitle>
-            </DialogHeader>
-            <DialogBody>
-              <p>{board.id}번 게시물 수정하시겠습니까?</p>
-            </DialogBody>
-            <DialogFooter>
-              <DialogActionTrigger>
-                <Button variant={"outline"}>취소</Button>
-              </DialogActionTrigger>
-              <Button
-                loading={progress}
-                colorPalette={"chan"}
-                onClick={handleSaveClick}
-              >
-                저장
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </DialogRoot>
-      </Stack>
-    </Box>
+      <Box>
+        <h3>{id}번 게시물 수정</h3>
+        <Stack gap={5}>
+          <Field label={"제목"}>
+            <Input
+                value={board.title}
+                onChange={(e) => setBoard({ ...board, title: e.target.value })}
+            />
+          </Field>
+          <Field label={"본문"}>
+            <Textarea
+                value={board.content}
+                onChange={(e) => setBoard({ ...board, content: e.target.value })}
+            />
+          </Field>
+          {hasAccess(board.writer) && (
+              <Box>
+                <DialogRoot
+                    open={dialogOpen}
+                    onOpenChange={(e) => setDialogOpen(e.open)}
+                >
+                  <DialogTrigger asChild>
+                    <Button
+                        disabled={disabled}
+                        colorPalette={"cyan"}
+                        variant={"outline"}
+                    >
+                      저장
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>저장 확인</DialogTitle>
+                    </DialogHeader>
+                    <DialogBody>
+                      <p>{board.id}번 게시물을 수정하시겠습니까?</p>
+                    </DialogBody>
+                    <DialogFooter>
+                      <DialogActionTrigger>
+                        <Button variant={"outline"}>취소</Button>
+                      </DialogActionTrigger>
+                      <Button
+                          loading={progress}
+                          colorPalette={"blue"}
+                          onClick={handleSaveClick}
+                      >
+                        저장
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </DialogRoot>
+              </Box>
+          )}
+        </Stack>
+      </Box>
   );
 }
--- frontend/src/page/board/BoardView.jsx
+++ frontend/src/page/board/BoardView.jsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { Box, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Field } from "../../components/ui/field.jsx";
 import { Button } from "../../components/ui/button.jsx";
@@ -15,11 +15,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../../components/ui/dialog.jsx";
+import {AuthenticationContext} from "../../components/content/AuthenticationProvider.jsx";
+
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
   const navigate = useNavigate();
+  const { hasAccess } = useContext(AuthenticationContext);
 
   useEffect(() => {
     axios.get(`/api/board/view/${id}`).then((res) => setBoard(res.data));
@@ -31,71 +34,74 @@ export function BoardView() {
 
   const handleDeleteClick = () => {
     axios
-      .delete(`/api/board/delete/${board.id}`)
-      .then((res) => res.data)
-      .then((data) => {
-        toaster.create({
-          type: data.message.type,
-          description: data.message.text,
+        .delete(`/api/board/delete/${board.id}`)
+        .then((res) => res.data)
+        .then((data) => {
+          toaster.create({
+            type: data.message.type,
+            description: data.message.text,
+          });
+          navigate("/");
+        })
+        .catch((e) => {
+          const data = e.response.data;
+          toaster.create({
+            type: data.message.type,
+            description: data.message.text,
+          });
         });
-        navigate("/");
-      })
-      .catch((e) => {
-        const data = e.response.data;
-        toaster.create({
-          type: data.message.type,
-          description: data.message.text,
-        });
-      });
   };
 
   return (
-    <Box>
-      <h3>{id} 번 게시물</h3>
-      <Stack gap={5}>
-        <Field label="제목" readOnly>
-          <Input value={board.title} />
-        </Field>
-        <Field label="본문" readOnly>
-          <Textarea value={board.content} />
-        </Field>
-        <Field label="작성자" readOnly>
-          <Input value={board.writer} />
-        </Field>
-        <Field label="작성일시" readOnly>
-          <Input value={board.inserted} type={"datetime-local"} />
-        </Field>
-
-        <Box>
-          <DialogRoot>
-            <DialogTrigger asChild>
-              <Button colorPalette={"red"}>삭제</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>삭제확인</DialogTitle>
-              </DialogHeader>
-              <DialogBody>
-                <p>{board.id}번 게시물 삭제하시겠습니까?</p>
-              </DialogBody>
-              <DialogFooter>
-                <DialogActionTrigger>
-                  <Button variant={"outline"}>취소</Button>
-                </DialogActionTrigger>
-                <Button colorPalette={"red"} onClick={handleDeleteClick}>
-                  삭제
+      <Box>
+        <h3>{id} 번 게시물</h3>
+        <Stack gap={5}>
+          <Field label="제목" readOnly>
+            <Input value={board.title} />
+          </Field>
+          <Field label="본문" readOnly>
+            <Textarea value={board.content} />
+          </Field>
+          <Field label="작성자" readOnly>
+            <Input value={board.writer} />
+          </Field>
+          <Field label="작성일시" readOnly>
+            <Input value={board.inserted} type={"datetime-local"} />
+          </Field>
+          {hasAccess(board.writer) && (
+              <Box>
+                <DialogRoot>
+                  <DialogTrigger asChild>
+                    <Button colorPalette={"red"} variant={"outline"}>
+                      삭제
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>삭제 확인</DialogTitle>
+                    </DialogHeader>
+                    <DialogBody>
+                      <p>{board.id}번 게시물을 삭제하시겠습니까?</p>
+                    </DialogBody>
+                    <DialogFooter>
+                      <DialogActionTrigger>
+                        <Button variant={"outline"}>취소</Button>
+                      </DialogActionTrigger>
+                      <Button colorPalette={"red"} onClick={handleDeleteClick}>
+                        삭제
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </DialogRoot>
+                <Button
+                    colorPalette={"cyan"}
+                    onClick={() => navigate(`/edit/${board.id}`)}
+                >
+                  수정
                 </Button>
-              </DialogFooter>
-            </DialogContent>
-          </DialogRoot>
-          <Button
-            colorPalette={"cyan"}
-            onClick={() => navigate(`/edit/${board.id}`)}
-          >
-            수정
-          </Button>
-        </Box>
-      </Stack>
-    </Box>
+              </Box>
+          )}
+        </Stack>
+      </Box>
   );
 }
