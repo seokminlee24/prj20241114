a1aeda2f471521b6d50f72fea3b9f9d3abd301d8
seokminlee24 <lsm67286564@gmail.com>
2024-11-20 14:15:20 +0900

    댓글 작성 후 toast

--- frontend/src/page/coment/CommentContainer.jsx
+++ frontend/src/page/coment/CommentContainer.jsx
@@ -1,12 +1,14 @@
-import {Stack} from "@chakra-ui/react";
-import {CommentInput} from "./CommentInput.jsx";
-import {CommentList} from "./CommentList.jsx";
-import {useEffect, useState} from "react";
+import { Box, Stack } from "@chakra-ui/react";
+import { CommentInput } from "./CommentInput.jsx";
+import { CommentList } from "./CommentList.jsx";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import {toaster} from "../../components/ui/toaster.jsx";
+
 
 export function CommentContainer({ boardId }) {
     const [commentList, setCommentList] = useState([]);
-    const [processing, setProcessing] = useState(false)
+    const [processing, setProcessing] = useState(false);
 
     useEffect(() => {
         if (!processing) {
@@ -18,29 +20,42 @@ export function CommentContainer({ boardId }) {
     }, [processing]);
 
     function handleSaveClick(comment) {
-        setProcessing(true)
-        axios.post("/api/comment/add", {
-            boardId: boardId,
-            comment: comment,
-        }).finally(()=>{
-            setProcessing(false)
-        });
+        setProcessing(true);
+        axios
+            .post("/api/comment/add", {
+                boardId: boardId,
+                comment: comment,
+            })
+            .then((res) => res.data.message)
+            .then((message) => {
+                toaster.create({
+                    type: message.type,
+                    description: message.text,
+                });
+            })
+            .finally(() => {
+                setProcessing(false);
+            });
     }
 
     function handleDeleteClick(id) {
-        setProcessing(true)
-        axios.delete(`/api/comment/remove/${id}`).finally(()=>{
-            setProcessing(false)
-        })
+        setProcessing(true);
+        axios.delete(`/api/comment/remove/${id}`).finally(() => {
+            setProcessing(false);
+        });
     }
 
-
-
-    return (<div>
-        <Stack gap={5}>
-            <h3>댓글</h3>
-            <CommentInput boardId={boardId} onSaveClick={handleSaveClick} />
-            <CommentList boardId={boardId} commentList={commentList} onDeleteClick={handleDeleteClick} />
-        </Stack>
-    </div>);
+    return (
+        <Box>
+            <Stack gap={5}>
+                <h3>댓글</h3>
+                <CommentInput boardId={boardId} onSaveClick={handleSaveClick} />
+                <CommentList
+                    boardId={boardId}
+                    commentList={commentList}
+                    onDeleteClick={handleDeleteClick}
+                />
+            </Stack>
+        </Box>
+    );
 }
