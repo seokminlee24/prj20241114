1ee48d0dfb5987c11e8432bc9752b6c45a515760
seokminlee24 <lsm67286564@gmail.com>
2024-11-22 17:41:49 +0900

    테이블 생성 쿼리 수정

--- sql/board.sql
+++ sql/board.sql
@@ -1,12 +1,12 @@
 # 게시물 테이블 생성
-USE prj20241114;
+
 
 CREATE TABLE board
 (
     id       INT PRIMARY KEY AUTO_INCREMENT,
     title    VARCHAR(300)  NOT NULL,
     content  VARCHAR(5000) NOT NULL,
-    writer   varchar(100)  NOT NULL,
+    writer   VARCHAR(20)   NOT NULL REFERENCES member (id),
     inserted DATETIME DEFAULT NOW()
 );
 
@@ -15,9 +15,13 @@ FROM board;
 
 # 페이지 연습용 복붙
 INSERT INTO board
-    (title, content, writer)
+(title, content, writer)
 SELECT title, content, writer
 FROM board;
 
 SELECT COUNT(*)
 FROM board;
+
+DESC board;
+
+DROP TABLE board;
--- sql/member.sql
+++ sql/member.sql
@@ -1,23 +1,20 @@
-use prj20241114;
-
+USE prj20241114;
 CREATE TABLE member
 (
     id          VARCHAR(20) PRIMARY KEY,
+    email       VARCHAR(300) UNIQUE,
     password    VARCHAR(30) NOT NULL,
     description VARCHAR(1000),
     inserted    DATETIME DEFAULT NOW()
 );
-
-select *
+SELECT *
 FROM member;
-
 
 DROP TABLE member;
 
-# ALTER TABLE member
-#     ADD COLUMN email VARCHAR(300) UNIQUE AFTER id;
-
 ALTER TABLE member
-    ADD COLUMN email VARCHAR(300) AFTER id;
+    ADD COLUMN email VARCHAR(300) UNIQUE AFTER id;
 
 DESC member;
+SELECT *
+FROM member;
